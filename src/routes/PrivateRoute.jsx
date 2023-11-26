4206d47469bb9caebb605d38a936671797dea8a1
sabrinara <you@example.com>
2023-11-26 21:27:11 +0600

    PrivateRouter Created and Modified

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
+
+const PrivateRouters = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+    console.log(location.pathname);
+
+    if (loading) {
+        return <span className="loading loading-bars loading-lg"></span>
+    }
+
+    if (user) {
+        return children;
+    }
+    return <Navigate to="/login" state={location.pathname} ></Navigate>
+    
+};
+
+export default PrivateRouters;
