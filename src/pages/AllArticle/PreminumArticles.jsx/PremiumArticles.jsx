ad90978048ad4863873d7c8c487d114d27a29629
sabrinara <you@example.com>
2023-11-30 22:12:18 +0600

    Premium Article card design change

--- src/pages/AllArticle/PreminumArticles.jsx/PremiumArticles.jsx
+++ src/pages/AllArticle/PreminumArticles.jsx/PremiumArticles.jsx
@@ -5,10 +5,8 @@ import { Link } from "react-router-dom";
 
 const PremiumArticles = () => {
     const [articles, setArticles] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [page, setPage] = useState(1);
-    const [hasMore, setHasMore] = useState(true);
-    const [isSubscribed, setIsSubscribed] = useState(true); // Set the initial value based on the user's subscription status
+
+
 
     useEffect(() => {
         const fetchArticles = async () => {
@@ -16,10 +14,7 @@ const PremiumArticles = () => {
                 const response = await fetch(`https://the-news-hunter-server-lac.vercel.app/news`);
                 const data = await response.json();
 
-                if (data.length === 0) {
-                    setHasMore(false);
-                    return;
-                }
+
 
                 // Filter articles with status 'premium'
                 const filteredArticles = data.filter(article => article.status === 'premium');
@@ -31,39 +26,38 @@ const PremiumArticles = () => {
 
         fetchArticles();
 
-    }, [page, searchTerm]);
+    }, []);
 
     return (
         <div>
             <HelmetKiller pagename="Premium Articles"></HelmetKiller>
             <div>
                 <h1 className="text-6xl font-bold text-center my-16">Premium Articles</h1>
-                
-                <div className="grid grid-cols-1 lg:grid-cols-2 px-20 gap-4">
+
+                <div className="grid grid-cols-1 lg:grid-cols-3 px-20 gap-4">
                     {articles.map((article, index) => (
                         <div key={index}>
-                            <div className="card  lg:h-[56vh] lg:card-side bg-base-100 shadow-xl m-6">
-                                <figure className="w-full h-[50vh] lg:h-[56vh] lg:w-[50%]">
-                                    <img className="w-full h-[50vh] lg:h-[56vh] lg:max-w-none" src={article.image} alt={article.title} />
+                            <div className="card w-96 shadow-xl image-full">
+                                <figure>
+                                <img className="w-full h-[50vh] lg:h-[56vh] lg:max-w-none" src={article.image} alt={article.title} />
                                 </figure>
-                                <div className="card-body lg:w-[50%]">
-                                    <h1 className="card-title text-3xl text-red-600">{article.title}.</h1>
-                                    <p className="text-sm overflow-hidden">{article.description}</p>
-
-                                    <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-start lg:gap-6">
+                                <div className="card-body">
+                                    <h2 className="card-title"></h2>
+                                    <h1 className="card-title text-3xl">{article.title}.</h1>
+                                    <div className="flex flex-col   justify-center items-center ">
                                         <img
                                             alt=""
                                             src={article.publisher.image}
-                                            className="relative inline-block h-[70px] w-[70px] lg:h-[50px] lg:w-[50px] rounded-full border-2 border-cyan-600 object-cover object-center"
+                                            className="relative inline-block h-[50px] w-[50px] rounded-full border-2 border-cyan-600 object-cover object-center"
                                         />
                                         <h5 className="mb-4 font-sans text-lg font-semibold ">
                                             {article.publisher.name}
                                         </h5>
                                     </div>
-                                    <div className="card-actions justify-center lg:justify-start">
-                                        <Link to={`/details/${article._id}`} className="lg:mt-2">
+                                    <div className="card-actions justify-center ">
+                                        <Link to={`/details/${article._id}`} className="">
                                             <button
-                                                className="px-4 py-2 rounded text-white hover:text-white bg-sky-600 hover:bg-sky-500"
+                                                className="px-4 py-2 rounded text-white hover:text-white bg-red-600 hover:bg-sky-500"
                                             >
                                                 View Details
                                             </button>
@@ -71,6 +65,7 @@ const PremiumArticles = () => {
                                     </div>
                                 </div>
                             </div>
+                            
                         </div>
                     ))}
                 </div>
