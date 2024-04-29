475ac54811e6f1959ec415b73492528e2c8a6679
sabrinara <saashid18@gmail.com>
2024-04-30 00:21:13 +0600

    about us page design

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,20 @@
 
+import Con from "../../../public/about.json";
+import Lottie from "lottie-react";
 
 const About = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col md:flex-row items-center justify-between mt-0">
+            <div className="">
+                <h1 className="text-3xl md:text-5xl font-bold text-cyan-600 mt-10 ">About Us</h1>
+                <p className="text-sm  my-2 text-cyan-600">Get in touch with us. We will respond as soon as possible.</p>
+                <p className="text-sm  my-2 text-cyan-600">Reach Out for any type of Inquiries, or Just to Say Hi!</p>
+                <p className="text-sm  my-2 text-cyan-600">Send us a message!</p>
+            </div>
+             <div className="w-full md:w-2/3 ">
+                    <Lottie animationData={Con} loop={true} />
+                </div>
+
         </div>
     );
 };
